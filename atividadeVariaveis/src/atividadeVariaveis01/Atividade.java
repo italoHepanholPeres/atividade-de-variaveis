33c11e0aeb001be9756dc54d1e135390aa32dbd4
italoHespanholPeres <pereshespanhol1@gmail.com>
2022-07-26 22:29:09 -0300

    projeto tipos primitivos

---
+++ atividadeVariaveis/src/atividadeVariaveis01/Atividade.java
@@ -0,0 +1,24 @@
+package atividadeVariaveis01;
+
+public class Atividade {
+
+	public static void main(String[] args) {
+
+		boolean valorLogico = true;
+		int valorIntero = 2;
+		String nomeAmigo = "lucas";
+		double valorReal = 23.5 ;
+		
+		
+		
+		
+		
+		// atividade de casa: declarar 10 variaveis de cada tipo com diferentes valores
+		// fazer um que calcule o valor de uma variavel do tipo int elevado ao quadrado
+		// = "atribui um valor a varivel
+		//leia sobre if, if else, else
+		
+
+	}
+
+}
